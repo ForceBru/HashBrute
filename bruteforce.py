9f7046919c1dcbc518524a2e5ade7c4c05aafef7
Mia <admin@brute.heliohost.org>
2014-09-14 17:44:21 +0400

    Added web address

--- bruteforce.py
+++ bruteforce.py
@@ -7,6 +7,8 @@
 # LEGAL penteration-testing, not for hacking. This programme is completely free and
 # opensource, so you can modify the code as you want.
 
+# If you are interested in bruteforce, visit  http://brute.heliohost.org 
+
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 # WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
